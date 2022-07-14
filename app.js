b55b130b802a0f4430f6991a4d1e4995ab8b36d1
Fagner202 <fagnersilveira86@gmail.com>
2022-07-13 21:42:38 -0300

    Costruindo funções das quatro operações da matematica

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ function soma (a, b) {
     return a + b
 };
 
+function subtracao (a, b) {
+    return a - b
+};
+
+var multiplicacao = a * b;
+
+var divisao = a / b
+
+function dividiMultiplicacao () {
+
+};
+
 console.log(soma(50, 100));
 
-// Adicionando em uma branch
+
